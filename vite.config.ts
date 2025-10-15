0064daf18e1fe97a1072198df96900832447c8a4
mooonlight545-netizen <mooonlight545@gmail.com>
2025-10-15 17:44:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: "/hm-42a9-preview/", // 👈 ADD THIS LINE (use your repo name here)
+  base: "/hm-42a9-preview/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
